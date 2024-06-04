69564a3f5164dbf98bb04dd984e30887e0e610ac
johannesAkse <johannes.akse@knowit.no>
2024-06-04 18:25:49 +0200

    PO-28 add task data and GET endpoint

--- parent-organizr/src/main/resources/createTaskData.py
+++ parent-organizr/src/main/resources/createTaskData.py
@@ -8,12 +8,12 @@ fake = faker.Faker()
 # Load parent IDs from parents.json
 with open('./data/parents.json', 'r') as f:
     parents = json.load(f)
-parent_ids = [parent['id'] for parent in parents]
+    parent_ids = [parent['id'] for parent in parents]
 
 # Load location IDs from locations.json
 with open('./data/locationData.json', 'r') as f:
     locations = json.load(f)
-location_ids = [location['id'] for location in locations]
+    location_ids = [location['id'] for location in locations]
 
 # Define a list of task names
 task_names = ["Trafikkvakt", "Cafe", "Basar", "10.klasse cafe", "Pizzaovn", "Riddertelt", "Rake løv", "male gjerde", "Lykkejul"]
@@ -22,18 +22,17 @@ task_names = ["Trafikkvakt", "Cafe", "Basar", "10.klasse cafe", "Pizzaovn", "Rid
 def generate_taskData():
 
     return {
-
-        "task_name": random.choice(task_names),
-        "leader_id": random.choice(parent_ids),
+        "taskName": random.choice(task_names),
+        "leader": random.choice(parent_ids),
         "location": random.choice(location_ids),
-        "description": fake.text(),
-        "date_updated": str(fake.date_time_this_year()),
-        "version": "1.0.0"
+        "description": fake.text(40),
+        "dateCreated": str(fake.date_time_this_year()),
+        "dateUpdated": str(fake.date_time_this_year())
     }
 
 # Generate 130 taskData objects
-taskDatas = [generate_taskData() for _ in range(130)]
+taskDatas = [generate_taskData() for _ in range(80)]
 
 # Write the taskData objects to a JSON file
-with open('./data/taskDatas.json', 'w') as f:
+with open('./data/taskData.json', 'w') as f:
     json.dump(taskDatas, f, indent=4)
---
+++ parent-organizr/src/test/java/TaskIntegrationTest.java
@@ -0,0 +1,30 @@
+import com.axeweb.parentorganizr.model.Task;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+ class TaskIntegrationTest {
+
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:alpine");
+
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @Test
+    void shouldFindAllTasks() {
+        // /api/tasks
+        Task[] tasks = restTemplate.getForObject("/tasks", Task[].class);
+        assertThat(tasks).hasSize(80);
+    }
+}
