7b60281b088386936f88ff7e749d8c406f229a90
johannesAkse <johannes.akse@knowit.no>
2024-06-05 08:50:44 +0200

    PO-28 Refactor dataLoader. Move all into one.

---
+++ parent-organizr/src/main/java/com/axeweb/parentorganizr/DataLoader.java
@@ -0,0 +1,91 @@
+package com.axeweb.parentorganizr;
+
+import com.axeweb.parentorganizr.model.Location;
+import com.axeweb.parentorganizr.model.Parent;
+import com.axeweb.parentorganizr.model.Task;
+import com.axeweb.parentorganizr.repository.LocationRepository;
+import com.axeweb.parentorganizr.repository.ParentRepository;
+import com.axeweb.parentorganizr.repository.TaskRepository;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+
+
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    private static final Logger log = LoggerFactory.getLogger(DataLoader.class);
+    private final ObjectMapper objectMapper;
+    private final LocationRepository locationRepository;
+    private final ParentRepository parentRepository;
+    private final TaskRepository taskRepository;
+    private static final String logText = "Loading data into database from json {} ";
+    private static final String errorText = "Failed to load data from json file: \n {}";
+
+    public DataLoader(ObjectMapper objectMapper, LocationRepository locationRepository, ParentRepository parentRepository, TaskRepository taskRepository) {
+        this.objectMapper = objectMapper;
+        this.locationRepository = locationRepository;
+        this.parentRepository = parentRepository;
+        this.taskRepository = taskRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        loadParents();
+        loadLocations();
+        loadTasks();
+    }
+    private void loadParents() {
+        long count = parentRepository.count();
+        log.info("Parent count in database: {}", count);
+        if(parentRepository.count() == 0) {
+            String PARENTS_JSON = "/data/parents.json";
+            log.info(logText,  PARENTS_JSON);
+            try (InputStream inputStream = TypeReference.class.getResourceAsStream(PARENTS_JSON)) {
+                Parent[] parents = objectMapper.readValue(inputStream, Parent[].class);
+                parentRepository.saveAll(Arrays.asList(parents));
+            } catch (IOException e) {
+                throw new RuntimeException(errorText, e);
+            }
+        }
+    }
+
+    private void loadLocations() {
+        long count = locationRepository.count();
+        log.info("Location count in database: {}", count);
+        if(locationRepository.count() == 0) {
+            String LOCATIONS_JSON = "/data/locationData.json";
+            log.info(logText,  LOCATIONS_JSON);
+            try (InputStream inputStream = TypeReference.class.getResourceAsStream(LOCATIONS_JSON)) {
+               Location[] locations = objectMapper.readValue(inputStream, Location[].class);
+                locationRepository.saveAll(Arrays.asList(locations));
+            } catch (IOException e) {
+                throw new RuntimeException(errorText, e);
+            }
+        }
+    }
+
+    private void loadTasks() {
+        long count = taskRepository.count();
+        log.info("Task count in database: {}", count);
+        if(taskRepository.count() == 0) {
+            String TASKS_JSON = "/data/taskData.json";
+            log.info(logText,  TASKS_JSON);
+            try (InputStream inputStream = TypeReference.class.getResourceAsStream(TASKS_JSON)) {
+               Task[] tasks = objectMapper.readValue(inputStream, Task[].class);
+                taskRepository.saveAll(Arrays.asList(tasks));
+            } catch (IOException e) {
+                throw new RuntimeException(errorText, e);
+            }
+        }
+    }
+
+
+}
